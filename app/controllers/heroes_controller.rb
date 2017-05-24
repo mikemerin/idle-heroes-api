b42f1f232d418541bcc3543ca5446855584ce748
Mike Merin <mikemerinweather@gmail.com>
2017-05-24 16:41:03 -0400

    base requirements completed

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -16,4 +16,29 @@ class HeroesController < ApplicationController
     end
   end
 
+  def new
+    @hero = Hero.new()
+  end
+
+  def create
+    @hero = Hero.new(hero_params)
+    @stat = Stat.new(stats_params["stats"])
+    @hero.stat = @stat
+    if @hero.save && @stat.save
+      redirect_to hero_path(@hero)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def hero_params
+    params.require(:hero).permit(:name, :faction, :role, :skills, :stats_id)
+  end
+
+  def stats_params
+    params.require(:hero).permit(:stats => [:health, :attack, :armor, :speed])
+  end
+
 end
